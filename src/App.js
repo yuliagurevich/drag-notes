b9f5d2cd0780a035345d50a341b6fb42ed28474f
Yulia <yulia.v.gurevich@gmail.com>
2019-10-02 23:32:21 +0300

    Added create a note.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      notes: [],
+    }
+  }
+
+  clone = (items) => items.map(item => Array.isArray(item) ? this.clone(item) : item);
+
+  renderNotes = (notes) => {
+    return notes.map((note) => {
+      return <div className="note"></div>
+    });
+  }
+
+  handleCreateNote = () => {
+    console.log(">>> got here");
+    
+    const { notes } = this.state;
+
+    let _notes = this.clone(notes);
+
+    _notes.unshift({
+      header: '',
+      note: '',
+    });
+
+    this.setState({
+      notes: _notes
+    }, () => console.log(this.state.notes));
+  }
+
+  render() {
+    const { notes } = this.state;
+
+    return (
+     <div id="container">
+       <div id="trash-can" >trash</div>
+       <div id="note-board">
+         {this.renderNotes(notes)}
+       </div>
+       <button id="add-note-button" onClick={this.handleCreateNote}>+</button>
+     </div>
+   );
+  }
 }
 
 export default App;
